222403a3536469d5c960dc4b34fcd83ca9a7e7c6
Light Focus <952646764@qq.com>
2018-04-11 22:03:43 +0800

    Fix the connection with MySQL using utf8.

--- pdo.php
+++ pdo.php
@@ -1,3 +1,4 @@
 <?php
- $pdo=new PDO('mysql:host=localhost;port=3306;dbname=accounts','root','root');
+ $pdo=new PDO('mysql:host=47.91.213.123;port=3306;dbname=accounts','root','dafuweng7');
+ $pdo->query("SET NAMES utf8");
  $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
